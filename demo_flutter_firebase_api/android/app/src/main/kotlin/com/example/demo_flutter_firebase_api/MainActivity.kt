5124213a12a8baa411f9e111d6ef8e2b19ad0dab
xuanhapham <phamthixuanha@secure.janeto.com>
2021-03-31 16:55:32 +0700

    no message

---
+++ demo_flutter_firebase_api/android/app/src/main/kotlin/com/example/demo_flutter_firebase_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_flutter_firebase_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo_flutter_firebase_api/functions/index.js
@@ -0,0 +1,28 @@
+const functions = require("firebase-functions");
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    const original = req.query.text;
+    const writeResult = await admin.firestore().collection('messages').add({ original: original });
+    res.json({ result: 'Message with ID: ${writeResult.id} added' });
+});
+
+
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+      const original = snap.data().original;
+      functions.logger.log('Uppercasing', context.params.documentId, original);
+      
+      const uppercase = original.toUpperCase();
+      return snap.ref.set({uppercase}, {merge: true});
+    });
